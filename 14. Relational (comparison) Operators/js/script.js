c77e200df9b18547217a0ad942818efe757e73a3
Washington Silva Ferreira Leite <58192745+washigtonSilvaFerrreiraLeite@users.noreply.github.com>
2023-01-10 20:45:19 -0300

    Add files via upload

---
+++ 14. Relational (comparison) Operators/js/script.js
@@ -0,0 +1,21 @@
+/*
+ * RELATIONAL OPERATORS (comparison operators)
+ * 
+ * ==
+ * 
+ * === - IDENTICAL, variables must be the same type too
+ * 
+ * !=
+ * 
+ * >
+ * <
+ * >=
+ * <=
+ * 
+ */
+
+var a = 7,
+    b = 5;
+    
+alert(a < 5);
+
